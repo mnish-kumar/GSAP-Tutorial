5a8ca80603812c571a4e8e0cb4681c48d4e95025
Manish Kumar <manishkr.smi@gmail.com>
2025-08-30 17:45:45 +0530

    Second Class

--- Medium/script.js
+++ Medium/script.js
@@ -1,4 +1,21 @@
-gsap.to("div", {
-    x:600,
-    duration:1
+gsap.from(".page1 .box", {
+    scale: 0,
+    delay:1,
+    duration: 2,
+    rotate: 360,
+    borderRadius: 50
+})
+
+gsap.from(".page2 .box", {
+    scale: 0,
+    duration: 2,
+    rotate: 360,
+    scrollTrigger: {
+        trigger: ".page2 .box",
+        scroller: "body",
+        markers: true,
+        start: "top 60%",
+        end: "top 30%",
+        scrub: 2
+    }
 })
